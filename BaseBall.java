0be7dcbaf66e27162fa002b863428f53d9068b65
donggulim <48562521+donggulim@users.noreply.github.com>
2019-12-02 12:07:49 +0900

    function 구현

---
+++ BaseBall.java
@@ -0,0 +1,69 @@
+package BaseBall;
+import java.util.*;
+
+public class BaseBall {
+
+	public static void main(String[] args) {
+		Play();
+	}
+	
+	
+	static int[] count = {0,0,0,0}; //스트라이크,볼,아웃,안타. 	
+	
+	static void Play() {
+		Run();
+		Check();
+	}
+	
+	private static void Run() {
+		Random generator = new Random(); 
+		while(count[2]<3) { 
+			int a = generator.nextInt(4);  //  0~3 랜덤넘버 생성.
+		if(a==0) {
+			System.out.println("스트라이크!");
+			count[0]++;
+			System.out.println(count[0]+"S "+count[1]+"B "+count[2]+"O");
+			}else if(a==1){
+			System.out.println("볼");
+			count[1]++;
+			System.out.println(count[0]+"S "+count[1]+"B "+count[2]+"O");
+			}else if(a==2) {
+			System.out.println("아웃! 다음 타자가 타석에 입장했습니다.");
+			count[0]=0; count[1]=0;
+			count[2]++;
+			System.out.println(count[0]+"S "+count[1]+"B "+count[2]+"O");
+			}else if(a==3) {
+			System.out.println("안타! 다음 타자가 타석에 입장했습니다.");
+			count[0]=0; count[1]=0;
+			count[3]++;
+			System.out.println(count[0]+"S "+count[1]+"B "+count[2]+"O");
+		}	
+	}
+}
+	
+	private static void Check() {
+		if(count[0]==3) {
+			count[0]=0;
+			count[2]++;  // 스트라이크3개 아웃+1
+		}
+		if(count[1]==4) {
+			count[1]=0;
+			count[3]++;  // 볼4 안타+1
+			count[3]++;  // 볼4 안타+1
+		}
+
+		if(count[2]==3) {
+				System.out.println("최종 안타수:"+ count[3]);
+				System.out.println("GAME OVER");
+		}
+		
+	}
+	
+	
+}
+
+			
+		
+	
+
+
